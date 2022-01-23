79327eefc3733919957f81c4fd3a343d0f1133af
crystalmonica1998 <crystalmonica.aboujneid@lau.edu>
2022-01-23 16:46:01 +0200

    rearranging part1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,8 @@ import { MoralisProvider } from "react-moralis";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MoralisProvider 
-      appId={process.env.NEXT_PUBLIC_APP_ID} 
-      serverUrl={process.env.NEXT_PUBLIC_APP_SERVER_URL}
+      appId="R3ZVwya1uXZszmo8vOYs8ul5yo4UVLG0f06sfVTv"
+      serverUrl="https://kcceuztnjtuf.usemoralis.com:2053/server"
     >
       <Component {...pageProps} />
     </MoralisProvider>
